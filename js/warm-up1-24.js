3242079a459007af5f17531a11cdad68d040b622
Manuel Dominguez <manuel.dominguez1113@gmail.com>
2022-01-24 09:31:59 -0600

    warm up

---
+++ js/warm-up1-24.js
@@ -0,0 +1,20 @@
+function sumOfAll(input){
+let inputArr= input.toString();
+inputArr=input.split('');
+let sum = inputArr.reduce((total,sum)=>
+     parseInt(total) + parseInt(sum));
+
+return sum;
+
+    /*    let x;
+    input.toString().split('');
+    input.length
+ for(let i=0;i<input.length; i++){
+     parseInt(input[i]);
+     x += input[i]; }*/
+
+   // return x;
+}
+
+console.log(sumOfAll('42958372'));
+console.log(sumOfAll('917485'));
